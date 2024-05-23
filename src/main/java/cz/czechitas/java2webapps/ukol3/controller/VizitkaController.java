fc81c3708635b20350102e3e5972172481e685a0
Linda Hrdinová <hrlin@seznam.cz>
2024-05-23 16:35:45 +0200

    Homework feedback 0.2

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -14,12 +14,12 @@ import java.util.List;
 @Controller
 public class VizitkaController {
     private final List<Vizitka> seznamVizitek = List.of(
-            new Vizitka("Dita (Přikrylová) Formánková", "Czechitas z. s.", "Václavské náměstí 837/11", "110 00", "dita@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
-            new Vizitka("Barbora Bühnová", "Czechitas z. s.", "Václavské náměstí 837/11", "110 00", null, "+420 800123456", "www.czechitas.cz"),
-            new Vizitka("Monika Ptáčníková", "Czechitas z. s.", "Václavské náměstí 837/11", "110 00", "monika@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
-            new Vizitka("Linda Hrdinová", "Buchtičkovi s. r. o.", "Na Kocourkách 6", "169 00", "hrdinova.linda@seznam.cz", "+420 789 123 456", "www.buchtickovi.cz"),
-            new Vizitka("Melichar von Buchtičkov", "Buchtičkovi s. r. o.", "Na Kocourkách 16/6", "169 00", null, null, "www.buchtickovi.cz"),
-            new Vizitka("Princezna Polly", "Buchtičkovi s. r. o.", "Na Kocourkách 16/6", "169 00", null, null, "www.buchtickovi.cz")
+            new Vizitka("Dita (Přikrylová) Formánková", "Czechitas z. s.", "Václavské náměstí 837/11", "110 00 Praha 1", "dita@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
+            new Vizitka("Barbora Bühnová", "Czechitas z. s.", "Václavské náměstí 837/11", "110 00 Praha 1", null, "+420 800123456", "www.czechitas.cz"),
+            new Vizitka("Monika Ptáčníková", "Czechitas z. s.", "Václavské náměstí 837/11", "110 00 Praha 1", "monika@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
+            new Vizitka("Linda Hrdinová", "Buchtičkovi s. r. o.", "Na Kocourkách 6", "169 00 Praha", "hrdinova.linda@seznam.cz", "+420 789 123 456", "www.buchtickovi.cz"),
+            new Vizitka("Melichar von Buchtičkov", "Buchtičkovi s. r. o.", "Na Kocourkách 16/6", "169 00 Praha", null, null, "www.buchtickovi.cz"),
+            new Vizitka("Princezna Polly", "Buchtičkovi s. r. o.", "Na Kocourkách 16/6", "169 00 Praha", null, null, "www.buchtickovi.cz")
     );
 
     @GetMapping("/")
